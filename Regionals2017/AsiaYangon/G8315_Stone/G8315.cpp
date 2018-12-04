df0467f376ae53303376f5193bb533208e7c1790
trunghe <33113755+trunghe@users.noreply.github.com>
2018-12-04 19:03:56 +0700

    The commented code are FAILED (TLE, RTE) soltutions!

---
+++ Regionals2017/AsiaYangon/G8315_Stone/G8315.cpp
@@ -0,0 +1,57 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//typedef long long ll;
+//typedef vector<int> vi;
+////typedef vector<vi> vvi;
+////typedef vector<ll> vl;
+////typedef vector<vl> vvl;
+////typedef vector<vvl> vvvl;
+////typedef pair<int, int> ii;
+////typedef vector<ii> vii;
+////typedef vector<vii> vvii;
+//
+//#define FOR(i, a, b) \
+//    for (int i = (a); i < (b); i++)
+//#define FORE(i, a, b) \
+//    for (int i = (a); i <= (b); i++)
+////#define FORD(i, a, b) \
+////    for (int i = (a); i >= (b); i--)
+//
+////#define INF 1e9+7
+////#define INFLL 1e18+7
+////#define esp 1e-9
+////#define PI 3.14159265
+////
+////ll nMod = 1e9 + 7;
+//
+////inline ll GCD(ll a, ll b) {while (b != 0) {ll c = a % b; a = b; b = c;} return a;};
+////inline ll LCM(ll a, ll b) {return (a / GCD(a,b)) * b;};
+//
+//int ceilIndex(vi &vec, int left, int right, int key) {
+//    int mid;
+//    while (right - left > 1) {
+//        mid = left + (right - left) / 2;
+//        if (vec[m] >= key) {
+//            right = mid;
+//        } else {
+//            left = mid;
+//        }
+//    }
+//    return right;
+//}
+//
+//int greatestIncreasingSubsequenceSum(vi &vec) {
+//
+//}
+//
+//int main() {
+////    ios_base::sync_with_stdio(false);
+////    cin.tie();
+////    freopen("C:\\CLionProjects\\ACM Regionals\\?\\?\\?\\?.in", "r", stdin);
+////    freopen("C:\\CLionProjects\\ACM Regionals\\?\\?\\?\\?.out", "w", stdout);
+////    freopen("C:\\CLionProjects\\ACM Regionals\\?\\?\\?\\?.err", "w", stderr);
+//    vector<int> vec;
+//    return 0;
+//}
